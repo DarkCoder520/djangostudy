b4dcd2ec515e83cf584a33610ee9c530edfbe378
tangyuan <tayetang@163.com>
2018-07-26 17:29:05 +0800

    remove

--- lawdataspider/lawdataspider/start.py
+++
@@ -1,5 +0,0 @@
-from scrapy import cmdline
-
-#cmdline.execute("scrapy crawl findlawspider".split())
-#cmdline.execute("scrapy crawl baidulawspider".split())
-cmdline.execute("scrapy crawlall".split())
--- taobaosplash/taobaosplash/start.py
+++
@@ -1,4 +0,0 @@
-from scrapy import cmdline
-
-#cmdline.execute("scrapy crawl taobaodressspider".split())
-cmdline.execute("scrapy crawlall".split())
