b0ea3b931214ef9087f5b7e18a6b5259d8d806ba
tyhp1 <1337369178@qq.com>
2018-07-28 10:41:20 +0800

    demo2

--- miniwebframe.py
+++ miniwebframe.py
@@ -5,9 +5,18 @@ sk = socket.socket()
 sk.bind(("127.0.0.1",8080))
 sk.listen(5)
 
+
 while True:
     conn,_ = sk.accept()
     data = conn.recv(8096)
+    request_params = str(data, encoding="utf-8")
+    url_paths = request_params.split("\r\n")[0]
+    url = url_paths.split()[1]
     conn.send(b"HTTP/1.1 200 OK\r\n\r\n")
-    conn.send(b"hello")
+    if url == "/tangyuan":
+        conn.send(b"tangyuan")
+    elif url == "/xiaotang":
+        conn.send(b"xiaotang")
+    else:
+        conn.send(bytes("404了",encoding="gbk"))
     conn.close()
